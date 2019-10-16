8de4b664ef2184a177654fea2660080babcf1259
Chuan <chuan.bai@gmail.com>
2019-10-16 13:03:55 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,8 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/f9x0l4xuyblxlyf/stage-4-.98.pth?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/f9x0l4xuyblxlyf/stage-4-.98.pth?raw=1'
+export_file_url = 'https://www.dropbox.com/s/4faa1q2bo9hx8nk/export.pkl?raw=1'
 
 export_file_name = 'export.pkl'
 
